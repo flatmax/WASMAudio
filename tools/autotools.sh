49af61e11696baed3f3bf03f6489c78ff780dea9
flatmax <flatmax@flatmax.org>
2018-03-07 15:07:35 +1100

    Copyright year update.

--- tools/autotools.sh
+++ tools/autotools.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright (c) 2017 The WASMAudio Authors. All rights reserved.
+# Copyright (c) 2017-2018 The WASMAudio Authors. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are
