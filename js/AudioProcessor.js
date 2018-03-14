cd93eae4d7618a688450d235119b590cc9b8d686
flatmax <flatmax@flatmax.org>
2018-03-14 11:53:14 +1100

    Documentation update.

--- js/AudioProcessor.js
+++ js/AudioProcessor.js
@@ -26,9 +26,8 @@
 // (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-/** A simple js based audio processor for testing AudioWorkletProcessor.
-The AudioWorkletProcessor's scope is too tight to srouce WASM from a url.
-This version doesn't work.
+/** A simple js based audio processor for running WASM processing using the
+AudioWorkletProcessor.
 */
 class AudioProcessor extends AudioWorkletProcessor {
   constructor(){
