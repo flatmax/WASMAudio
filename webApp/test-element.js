51a7a9f5904145ef45a4f74053ae0098e440a933
Matt Flax <flatmax@flatmax.org>
2018-10-15 11:13:33 +1100

    Completed the upgrade to polymer 3

--- webApp/test-element.js
+++ webApp/test-element.js
@@ -1,4 +1,4 @@
-<!--
+/**
 Copyright (c) 2017-2018 The WASM audio Authors. All rights reserved.
 
  Redistribution and use in source and binary forms, with or without
@@ -26,49 +26,46 @@ Copyright (c) 2017-2018 The WASM audio Authors. All rights reserved.
  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
--->
+*/
 
-<link rel="import" href="../paper-button/paper-button.html">
+import {html, PolymerElement} from '@polymer/polymer/polymer-element.js';
+import '@polymer/paper-button';
 
-<dom-module id="test-element">
-  <template>
-    <paper-button raised on-tap="runAudioWorklet">run worklet</paper-button>
-  </template>
+/**
+ * `test-element`
+ * WASM AudioWorklet tester
+ *
+ * @customElement
+ * @polymer
+ * @demo demo/index.html
+ */
+class TestElement extends PolymerElement {
+  static get template() {
+    return html`
+    	<paper-button raised on-tap="runAudioWorklet">run worklet</paper-button>
+    `;
+  }
 
-  <script>
-    /**
-     * `test-element`
-     * WASM AudioWorklet tester
-     *
-     * @customElement
-     * @polymer
-     * @demo demo/index.html
-     */
-    class TestElement extends Polymer.Element {
-      static get is() { return 'test-element'; }
+  /** Run the audio worklet processor
+  */
+  runAudioWorklet(){
+    if (this.context==null)
+      this.context = new AudioContext();
+    this.context.audioWorklet.addModule('libwasmaudio.js').then(() => {
+      let oscillator = new OscillatorNode(this.context);
+      let oscillator2 = new OscillatorNode(this.context);
+      oscillator2.frequency.setValueAtTime(1000, this.context.currentTime);
+      let audioWorkletNode = new AudioWorkletNode(this.context, 'audio-processor', {'numberOfInputs' : 3, 'numberOfOutputs' : 2 });
+      let merger =  new ChannelMergerNode(this.context, {'numberOfInputs' : 2});
+      // connect the oscillator to the AudioWorkletNode WASM audio processor
+      oscillator.connect(audioWorkletNode, 0, 0);
+      oscillator2.connect(audioWorkletNode, 0, 1);
+      audioWorkletNode.connect(merger, 0, 0) // connect the L + R + destination
+      audioWorkletNode.connect(merger, 1, 1).connect(this.context.destination);
+      oscillator.start(); // start the oscillator
+      oscillator2.start(); // start the oscillator
+    });
+  }
+}
 
-      /** Run the audio worklet processor
-      */
-      runAudioWorklet(){
-        if (this.context==null)
-          this.context = new AudioContext();
-        this.context.audioWorklet.addModule('libwasmaudio.js').then(() => {
-          let oscillator = new OscillatorNode(this.context);
-          let oscillator2 = new OscillatorNode(this.context);
-          oscillator2.frequency.setValueAtTime(1000, this.context.currentTime);
-          let audioWorkletNode = new AudioWorkletNode(this.context, 'audio-processor', {'numberOfInputs' : 3, 'numberOfOutputs' : 2 });
-          let merger =  new ChannelMergerNode(this.context, {'numberOfInputs' : 2});
-          // connect the oscillator to the AudioWorkletNode WASM audio processor
-          oscillator.connect(audioWorkletNode, 0, 0);
-          oscillator2.connect(audioWorkletNode, 0, 1);
-          audioWorkletNode.connect(merger, 0, 0) // connect the L + R + destination
-          audioWorkletNode.connect(merger, 1, 1).connect(this.context.destination);
-          oscillator.start(); // start the oscillator
-          oscillator2.start(); // start the oscillator
-        });
-      }
-    }
-
-    window.customElements.define(TestElement.is, TestElement);
-  </script>
-</dom-module>
+window.customElements.define('test-element', TestElement);
